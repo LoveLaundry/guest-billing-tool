3d590ea4e09b16e2fa1091274fbc899b49ff80e2
DulshanSiriwardhana <dulshansiriwardhanaofficial@gmail.com>
2026-09-21 00:40:02 +0530

    bill: secondary number is +94 70 243 3566

--- src/components/bill/BillHeader.tsx
+++ src/components/bill/BillHeader.tsx
@@ -24,7 +24,7 @@ const BillHeader=()=>{
             <div className="w-full h-fit flex justify-between items-center text-xs">
                 <div>
                     <div>
-                        Tel: +94 77 4200 919 / 071-2978922
+                        Tel: +94 77 4200 919 / 070 243 3566
                     </div>
                     <div>Email: lovelaundry01@gmail.com</div>
                 </div>
